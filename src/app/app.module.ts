12b94f6ce0a1ba63079738aaddb3b314f90e4759
Shuma Shinozaki <shu.shinozaki@gmail.com>
2020-04-02 16:17:44 +0900

    ng generate component ckeditor5

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,21 +1,25 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FlexLayoutModule } from '@angular/flex-layout';
-import { CKEditorModule } from 'ckeditor4-angular';
+import { CKEditorModule as CKEditor4Module } from 'ckeditor4-angular';
+import { CKEditorModule as CKEditor5Module } from '@ckeditor/ckeditor5-angular';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { Ckeditor4Component } from './ckeditor4/ckeditor4.component';
+import { Ckeditor5Component } from './ckeditor5/ckeditor5.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    Ckeditor4Component
+    Ckeditor4Component,
+    Ckeditor5Component
   ],
   imports: [
     BrowserModule,
     FlexLayoutModule,
-    CKEditorModule,
+    CKEditor4Module,
+    CKEditor5Module,
     AppRoutingModule
   ],
   providers: [],
