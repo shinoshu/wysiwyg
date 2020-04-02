7b88260a373610dded1b89ca090ae29e465df55d
Shuma Shinozaki <shu.shinozaki@gmail.com>
2020-04-02 15:50:15 +0900

    Add CKEditor

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import { Ckeditor4Component } from './ckeditor4/ckeditor4.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'ckeditor4', component: Ckeditor4Component }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { CKEditorModule } from 'ckeditor4-angular';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -12,6 +13,7 @@ import { Ckeditor4Component } from './ckeditor4/ckeditor4.component';
   ],
   imports: [
     BrowserModule,
+    CKEditorModule,
     AppRoutingModule
   ],
   providers: [],
