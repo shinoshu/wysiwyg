c6285df20e0ac2794d55bbfe32505a6b34782b91
Shuma Shinozaki <shu.shinozaki@gmail.com>
2020-04-02 16:41:45 +0900

    Update Ckeditor5Component

--- src/app/ckeditor5/ckeditor5/ckeditor5.component.ts
+++ src/app/ckeditor5/ckeditor5/ckeditor5.component.ts
@@ -1,15 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+
+import * as ClassicEditor from '@ckeditor/ckeditor5-build-classic';
 
 @Component({
   selector: 'app-ckeditor5',
   templateUrl: './ckeditor5.component.html',
   styleUrls: ['./ckeditor5.component.css']
 })
-export class Ckeditor5Component implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+export class Ckeditor5Component {
+  public Editor = ClassicEditor;
 }
