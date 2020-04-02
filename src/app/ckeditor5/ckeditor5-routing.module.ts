c2b6ec258f846fb7ec370a3b18529d6266779000
Shuma Shinozaki <shu.shinozaki@gmail.com>
2020-04-02 16:48:54 +0900

    Update Ckeditor5Module

--- src/app/ckeditor5/ckeditor5-routing.module.ts
+++ src/app/ckeditor5/ckeditor5-routing.module.ts
@@ -1,12 +1,15 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { Routes, RouterModule } from '@angular/router';
 
+import { Ckeditor5Component } from './ckeditor5/ckeditor5.component';
 
+const routes: Routes = [
+  { path: '', component: Ckeditor5Component }
+];
 
 @NgModule({
   declarations: [],
-  imports: [
-    CommonModule
-  ]
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule],
 })
 export class Ckeditor5RoutingModule { }
--- src/app/ckeditor5/ckeditor5.module.ts
+++ src/app/ckeditor5/ckeditor5.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CKEditorModule } from '@ckeditor/ckeditor5-angular';
 
+import { Ckeditor5RoutingModule } from './ckeditor5-routing.module';
 import { Ckeditor5Component } from './ckeditor5/ckeditor5.component';
 
 @NgModule({
@@ -9,6 +10,7 @@ import { Ckeditor5Component } from './ckeditor5/ckeditor5.component';
   imports: [
     CommonModule,
     CKEditorModule,
+    Ckeditor5RoutingModule,
   ]
 })
 export class Ckeditor5Module { }
