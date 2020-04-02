0b49df223d10c643c0c85a3326d57141a7f6814c
Shuma Shinozaki <shu.shinozaki@gmail.com>
2020-04-02 16:08:57 +0900

    Update Ckeditor4Component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FlexLayoutModule } from '@angular/flex-layout';
 import { CKEditorModule } from 'ckeditor4-angular';
 
 import { AppRoutingModule } from './app-routing.module';
@@ -13,6 +14,7 @@ import { Ckeditor4Component } from './ckeditor4/ckeditor4.component';
   ],
   imports: [
     BrowserModule,
+    FlexLayoutModule,
     CKEditorModule,
     AppRoutingModule
   ],
